5babbbc821b2a7f3ceae1073665c516f2e822891
Pratik1Mundhe <82946330+Pratik1Mundhe@users.noreply.github.com>
2022-06-11 16:33:42 +0530

    Add files via upload

---
+++ functions/integrate.js
@@ -0,0 +1,201 @@
+const reactDom = require("react-dom");
+
+var type1 = document.getElementById("form1");
+function function1() {
+
+var div4  = document.getElementById("area4");
+ 
+reactDom.render(
+    div4.innerHTML = <p>asdfasdf121221</p>
+
+)
+
+ 
+}
+
+
+type1.onclick = function1;
+
+
+let type2 = document.getElementsByClassName("form2");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type3 = document.getElementsByClassName("form3");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type4 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type5 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type6 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type7 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type8 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type9 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type10 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type11 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type12 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type13 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type14 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type15 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type16 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type17 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type18 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type19 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type20 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type21 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type22 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type23 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type24 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type25 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type26 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
+let type27 = document.getElementsByClassName("form1");
+
+addEventListener("click",()=>{
+    
+
+
+})
